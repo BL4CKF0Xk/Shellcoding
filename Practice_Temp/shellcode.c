ca3413bc23855de0f9416b87bf21272ffe034ced
BL4CKF0XK <68717754+BL4CKF0Xk@users.noreply.github.com>
2024-05-10 10:36:47 +0530

    Update shellcode.c

--- Practice_Temp/shellcode.c
+++ Practice_Temp/shellcode.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned char code[] = \
-"SHELLCODE";
-
 main()
 {
+  unsigned char code[] = \
+"SHELLCODE";
+  
   printf("Shellcode Lenght: %d\n",(int)strlen(code));
   int (*ret)() = (int(*)())code;
 
